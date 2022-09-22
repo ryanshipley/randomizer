b95ec94681e6f4b704d34bc22083c58210b53f71
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-22 13:14:06 -0400

    renders button

---
+++ src/components/Button.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Button(){
+    return(
+        <div>
+            <form>
+                <button type="button">
+                    Cute Dogs!!
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default Button;
